350d470ed5cb91ef2aab1fa3981b638bbf77916b
Azimet Kerem Ersoy <azimetkeremersoy@gmail.com>
2023-03-22 01:46:01 +0300

    create post added2

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -29,7 +29,7 @@ export default function Header() {
   const username = userInfo?.username;
 
   return (
-    <header className="flex items-center justify-between mt-8 mb-16 mx-24">
+    <header className="flex items-center justify-between  py-8 mb-8 px-24 bg-gray-600 text-white">
       <Link to="/" className="text-4xl font-bold">
         MyBlog
       </Link>
@@ -38,6 +38,7 @@ export default function Header() {
           <>
             <Link to="/create">Create New Post</Link>
             <a onClick={logout}>Logout</a>
+            {username}
           </>
         )}
         {!username && (
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -2,31 +2,62 @@ import React from "react";
 
 export default function Post() {
   return (
-    <div className="flex mx-48 my-10 gap-10">
-      <div className="w-[650px]">
-        <img
-          src="https://miro.medium.com/v2/resize:fit:828/0*af6qZrOt3hXhUqYc"
-          alt=""
-        />
-      </div>
-      <div>
-        <h2 className="text-3xl font-bold">
+    <div className="lg:flex mx-auto my-12 gap-6 w-3/4 bg-gray-100">
+      <img
+        src="https://miro.medium.com/v2/resize:fit:828/0*af6qZrOt3hXhUqYc"
+        alt=""
+        className="xl:w-3/12 lg:w-4/12 object-fill"
+      />
+
+      <div className="p-4">
+        <h1 className="text-3xl font-bold lg:mt-0 mt-4">
           How to Fetch API Data in React Like a Pro
-        </h2>
-        <p className="mt-2">
-          <a className="text-lg mr-6 text-gray-600" href="">
-            kerem ersoy
-          </a>
-          <time>2023-03-21 10:40</time>
-        </p>
-        <p className="mt-2 text-lg ">
+        </h1>
+        <p className="mt-4">
           How This Simple Technique Will Boost User Experience? The technique we
           are going to talk about is known as render as you fetch. This simple
           technique is also suggested by React itself while fetching data.How
           This Simple Technique Will Boost User Experience? The technique we are
-          going to talk about is known as render as you fetch.
+          going to talk about is known as render as you fetch.How This Simple
+          Technique Will Boost User How This Simple Technique Will Boost User
+          Experience? as render as you fetch.How This Simple Technique Will
+          Boost User How This Simple Technique Will Boost User Experience?
+        </p>
+        <p className="flex gap-6 mt-4 text-gray-500">
+          <a href="">kerem ersoy</a>
+          <time>2023-03-21 10:40</time>
         </p>
       </div>
     </div>
+
+    // <div className="flex mx-48 my-10 gap-10">
+    //   <div className="w-[500px] ">
+    //     <img
+    //       src="https://miro.medium.com/v2/resize:fit:828/0*af6qZrOt3hXhUqYc"
+    //       alt=""
+    //     />
+    //   </div>
+    //   <div>
+    //     <h2 className="text-3xl font-bold">
+    //       How to Fetch API Data in React Like a Pro
+    //     </h2>
+    //     <p className="mt-2">
+    //       <a className="text-lg mr-6 text-gray-600" href="">
+    //         kerem ersoy
+    //       </a>
+    //       <time>2023-03-21 10:40</time>
+    //     </p>
+    //     <p className="mt-2">
+    //       How This Simple Technique Will Boost User Experience? The technique we
+    //       are going to talk about is known as render as you fetch. This simple
+    //       technique is also suggested by React itself while fetching data.How
+    //       This Simple Technique Will Boost User Experience? The technique we are
+    //       going to talk about is known as render as you fetch.How This Simple
+    //       Technique Will Boost User How This Simple Technique Will Boost User
+    //       Experience? as render as you fetch.How This Simple Technique Will
+    //       Boost User How This Simple Technique Will Boost User Experience?
+    //     </p>
+    //   </div>
+    // </div>
   );
 }
